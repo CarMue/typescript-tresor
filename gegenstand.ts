4b82a12d2a8e6803362f86bc9f3c2f1f4b0d8382
CarMue <c.mueller@schule.bremen.de>
2021-12-08 14:46:49 +0100

    Erklaerungen in gegenstand.ts ueberarbeitet

--- gegenstand.ts
+++ gegenstand.ts
@@ -1,16 +1,16 @@
+ export abstract class Gegenstand {
 /**
- * Schlüsselwort export notwendig, damit die Klasse auch außerhalb des
- * Moduls verfügbar ist. (Zugriff von außen z. B. über die index.html)
+ * Schlüsselwort export notwendig, damit die Klasse auch außerhalb eines
+ * Moduls verfügbar ist. (Zugriff von außen, z. B. über die index.html)
  * 
- * =====================================================================
  * Standardmäßige Sichtbarkeit von Klassen, Eigenschaften und Methoden:
  * Wird keiner der Zugriffsmodifizierer public, private oder protected
  * (nicht für Klassen möglich) angegebenen, ist die Sichtbarkeit
  * grundsätzlich public.
  */
- export abstract class Gegenstand {
 
-    /**
+  constructor(public readonly id: number, public wert: number) {
+  /**
    * Konstruktor, dessen Parameter im Falle einer Deklaration als public
    * (wie hier) oder private zugleich Klassenattribute darstellen. Die 
    * Deklaration als public oder private ist also vonnöten, wenn ein Parameter
@@ -22,34 +22,50 @@
    * soll, werden als public deklariert. Attribute, auf die nur lesend
    * zugegriffen werden soll, werden als public readonly deklariert.
    * 
+   * 
    * Begründung:
+   * ===========
    * Muss später ein Refactoring in dem Sinne vorgenommen werden, dass eines 
    * der Attribute doch auf private gesetzt werden muss, aber z. B. ein 
    * lesender Zugriff aus einer anderen Klasse erhalten bleiben soll,
-   * ließe sich ein Getter erzeugen, der genauso heißt, wie das
+   * ließe sich ein Getter für eine Property erzeugen, der genauso heißt, wie das
    * Klassenattribut hieß, als es noch public war. Der Getter wird nämlich,
    * anders als in Java, einzig über dessen Namen angesprochen - also ohne
-   * zusätzlich eine Methodenklammer mit aufzuführen. Das nun private
-   * Klassenattribut wäre dann lediglich vorher umzubennen, z. B. 'wert'
-   * in '_wert'.
+   * zusätzlich eine Methodenklammer mit aufzuführen.
    * 
-   * Beispiel
-   * 1. Klassenattribut 'wert' von public auf private ändern
-   * 2. Umbenennung des Klassenattributs 'wert' in '_wert'
-   * 3. ggf. Anlegen von Getter u./o. Setter 'wert'
+   * Das nun private Klassenattribut wäre dann lediglich vorher umzubennen,
+   * z. B. 'wert' in '_wert'.
+   * 
+   * Voraussetzung ist dabei, dass der Getter keine klassische Methode ist,
+   * sondern eine Property liefert. Dies wird erreicht, indem das 'get' dem
+   * Namen des Attributs inklusive eines Leerzeichens(!) vorangestellt wird. -->
+   * a) Getter für die Property Wert: get wert: number (return this._wert;)
+   * <-- Perfekt!
+   * b) Methode: getWert: number (return this._wert;)
+   * <-- Hier ungeeignet!
+   * 
+   * 
+   * Beispiel:
+   * =========
+   * 1. Klassenattribut 'wert' soll von public auf private geändert werden.
+   * 2. Umbenennung des Klassenattributs vornehmen, z. B. 'wert' in '_wert'
+   * 3. Für weiter oder später notwendige Zugriffe auf die Property '_wert':
+   *    Anlegen von Getter u./o. Setter 'wert'
    * ==>
    *   Konstruktor:
    *      constructor(public readonly id: number, private _wert: number) {}
    *   Getter für '_wert':
-   *      get wert() {return this._wert;}
+   *      get wert(): number {return this._wert;}
    *   Setter für '_wert':
-   *      set wert(wert: number) {this._wert = wert;}
+   *      set wert(wert: number): void {this._wert = wert;}
    *   Beispiel für Aufruf des Getters:
    *      let summe: number = 100 + this.wert;
    *   <-- Aufruf erfolgt allein über den Namen und OHNE KLAMMER!
    * 
    * =====================================================================
+   * 
    * integrierte Datentypen in Typescript:
+   * =====================================
    * Es gibt nur
    * * any (als Obertyp der folgenden 3 Datentypen:)
    * * number
@@ -57,7 +73,7 @@
    * * boolean
    * 
    */
-  constructor(public readonly id: number, public wert: number) {}
+  }
 
   toString(): string {
     /**
